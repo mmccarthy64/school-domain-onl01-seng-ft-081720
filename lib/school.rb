fc2d3b682966f08b23d3372c9ddcaaffb6deaf9c
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-29 19:17:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    
+  end
+  
+  def grade
+    
+  end
+    
+  
+end
