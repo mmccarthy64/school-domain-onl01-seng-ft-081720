92b93547f995d5452f751ac8963ffdecf2413f0f
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-29 19:56:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,11 @@ class School
   end
   
   def add_student(student_name, grade)
-    roster[grade] ||= []
-    roster[grade] << student_name
+    if roster[grade]
+      roster[grade] << student_name
+    else
+      roster[grade] = [name]
+    end
   end
   
   
